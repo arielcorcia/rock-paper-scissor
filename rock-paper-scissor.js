68f0dedcccf41c0a5e346d067bdd0f8b9951e231
Ariel Corcia <107890906+arielcorcia@users.noreply.github.com>
2024-03-14 20:56:26 -0400

    Add playGame function

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3)
+    let computerChoice = Math.floor(Math.random() * 3);
     switch(computerChoice) {
         case 0:
             return "rock";
@@ -12,14 +12,14 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
     
     if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
-        return `You win! ${toCapitalize(playerSelection)} beats ${toCapitalize(computerSelection)}.`
+        return `You win! ${toCapitalize(playerSelection)} beats ${toCapitalize(computerSelection)}.`;
     } else if (playerSelection == computerSelection) {
-        return `It's a tie! ${toCapitalize(playerSelection)} and ${toCapitalize(computerSelection)} are the same.`
+        return `It's a tie! ${toCapitalize(playerSelection)} and ${toCapitalize(computerSelection)} are the same.`;
     } else {
-        return `You lose! ${toCapitalize(computerSelection)} beats ${toCapitalize(playerSelection)}.`
+        return `You lose! ${toCapitalize(computerSelection)} beats ${toCapitalize(playerSelection)}.`;
     }
 }
 
@@ -27,7 +27,36 @@ function toCapitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+function playGame(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt(message = "Rock, paper or scissors?");
+        let computerSelection = getComputerChoice();
+        let round = playRound(playerSelection, computerSelection)
+        console.log(`${round}`)
+        
+        switch(round.charAt(4)) {
+            case "w": 
+                playerScore++;
+                break;
+            case "l":
+                computerScore++;
+                break;
+        }
+        
+        if (i == 4) {
+            if (playerScore > computerScore){
+                    console.log(`You win! ${playerScore}:${computerScore}`)
+                } else if (computerScore > playerScore) {
+                    console.log(`You lose! ${computerScore}:${playerScore}`)
+                } else {
+                    console.log(`Its a tie ${computerScore}:${playerScore}`)
+                }
+        }
+    }
+}
+
+playGame();
