b4fea5d458eb6895177064dc0cc02d00a78f61f3
Ariel Corcia <107890906+arielcorcia@users.noreply.github.com>
2024-03-14 20:01:14 -0400

    Add playRound and toCapitalize function

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -7,6 +7,27 @@ function getComputerChoice() {
         case 1:
             return "paper";
         case 2:
-            return "scissor";
+            return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    
+    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+        return `You win! ${toCapitalize(playerSelection)} beats ${toCapitalize(computerSelection)}.`
+    } else if (playerSelection == computerSelection) {
+        return `It's a tie! ${toCapitalize(playerSelection)} and ${toCapitalize(computerSelection)} are the same.`
+    } else {
+        return `You lose! ${toCapitalize(computerSelection)} beats ${toCapitalize(playerSelection)}.`
+    }
+}
+
+function toCapitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
